b7666ed756e5af26835b8539a12e184c6f40ca99
Paul Williams <p.b.williams@outlook.com>
2023-11-08 10:19:04 +0000

    cfg

--- webui-user - 3444.bat
+++ webui-user - 3444.bat
@@ -5,10 +5,6 @@ set GIT=
 set VENV_DIR=
 REM set COMMANDLINE_ARGS=--lowvram --precision full --no-half --skip-torch-cuda-test
 REM set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all --xformers --api
-REM set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all
-
-
-set CUDA_VISIBLE_DEVICES=""
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all --upcast-sampling --opt-sdp-attention --update-check
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all --port 3444
 
 call webui.bat
